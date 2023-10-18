4a21ad96b3a4742c4aeaa76e9bce35e3dfbcfdb1
Mirsadizada Burhanaddin <burhanaddinzm@code.local>
2023-10-18 17:45:57 +0400

    Shop.App

---
+++ Shop.App/Program.cs
@@ -0,0 +1,37 @@
+﻿using Shop.Core.Models;
+
+Product[] products = { };
+
+CreateProduct();
+void CreateProduct()
+{
+    Product product = new Product();
+
+    Console.WriteLine("Adi daxil edin");
+    product.Name = Console.ReadLine();
+    CheckString(product.Name);
+
+    Console.WriteLine("Kateqoriyani daxil edin");
+    product.Category = Console.ReadLine();
+    CheckString(product.Category);
+
+    Console.WriteLine("Qiymeti daxil edin");
+    product.Price = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Stok sayini daxil edin");
+    product.StockCount = int.Parse(Console.ReadLine());
+
+
+
+    Array.Resize(ref products, products.Length + 1);
+}
+
+void CheckString(string input)
+{
+
+    while (string.IsNullOrWhiteSpace(a))
+    {
+        Console.WriteLine("Input value is not correct");
+        a= Console.ReadLine();
+    }
+}
---
+++ Shop.Core/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace Shop.Core.Models;
+
+public class Product
+{
+    public string Name { get; set; }
+    public int Price { get; set; }
+    public string Category { get; set; }
+    public int StockCount { get; set; }
+
+    public void GetFullInfo(string name, string category, int price, int stockcount)
+    {
+        Console.WriteLine($"Name:{Name} Category:{Category} Price:{Price} Stock Count:{StockCount}");
+
+    }
+
+}
